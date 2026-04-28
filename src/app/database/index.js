0e2d68581f17b451da426103502d62e2b6cb4f6e
gledson <gledsonalcantara4@gmail.com>
2026-04-28 13:08:27 -0400

    index.js atualizado resolvido o problema de conexão

--- src/app/database/index.js
+++ src/app/database/index.js
@@ -4,27 +4,34 @@ import User from "../models/User.js";
 import Wallet from "../models/Wallet.js";
 import Transaction from "../models/Transaction.js";
 
-const Models = [User, Wallet,Transaction];
+// 1. Corrigido para "models" (minúsculo) para bater com o uso abaixo
+const models = [User, Wallet, Transaction];
 
 class Database {
   constructor() {
     this.init();
     this.connectionTest();
   }
-init() {
-    this.connection = databaseConfig.use_env_variable
-      ? new Sequelize(
-          process.env[databaseConfig.use_env_variable],
-          databaseConfig
-        )
-      : new Sequelize(databaseConfig);
 
+  init() {
+    // 2. Identifica qual ambiente estamos usando (no Render será 'production')
+    const env = process.env.NODE_ENV || "development";
+    const config = databaseConfig[env];
+
+    // 3. Inicializa a conexão usando o bloco correto (com SSL e Dialect)
+    if (config.use_env_variable) {
+      this.connection = new Sequelize(process.env[config.use_env_variable], config);
+    } else {
+      this.connection = new Sequelize(config);
+    }
+
+    // 4. Inicializa os modelos
     models.map((model) => model.init(this.connection));
 
+    // 5. Faz as associações entre tabelas (ex: User tem muitas Wallets)
     models.map(
       (model) =>
-        model.associate &&
-        model.associate(this.connection.models)
+        model.associate && model.associate(this.connection.models)
     );
   }
 
