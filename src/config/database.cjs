3d37996d30b61f1bc070a1242f3171b8e0e7ce86
gledson <gledsonalcantara4@gmail.com>
2026-04-28 12:56:36 -0400

    atualização nas variaves de conexão

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,25 +1,34 @@
 require("dotenv").config();
 
-module.exports = process.env.DATABASE_URL
-  ? {
-      use_env_variable: "DATABASE_URL",
-      dialect: "postgres",
-      define: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true,
+module.exports = {
+  // Configuração para quando estiver rodando no Render (Produção)
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
       },
-    }
-  : {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT || 5432,
-      dialect: "postgres",
-      define: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true,
-      },
-    };
+    },
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+  // Configuração para quando estiver rodando na sua máquina (Local)
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+};
